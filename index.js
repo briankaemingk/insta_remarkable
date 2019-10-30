688283451d659fe073c46bdc92683157743d3140
brian kaemingk <brian.e.k@gmail.com>
2019-10-29 18:15:15 -0700

    Testing removal of S3

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const scrapeIt = require("scrape-it")
-const request = require('request')
-const fs = require('fs')
 const slugify = require('slugify')
 const exec = require('child_process').exec
 var express = require('express');
 var wkhtmltopdf = require('wkhtmltopdf');
 const nodemailer = require('nodemailer');
-var AWS = require("aws-sdk");
-// Set the region
-AWS.config.update({region: 'us-west-2'});
-
-// Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 
 require('dotenv').config()
@@ -35,6 +27,9 @@ function os_func() {
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
+                if (`${error}`.includes('entry already exists')){
+                    console.log("*****EXISTS")
+                }
                 return;
             }
 
@@ -51,6 +46,13 @@ app.get('/', function (req, res) {
     instapaper_to_pdf();
     res.send('Complete')
 });
+
+app.get('/send', function (req, res) {
+    console.log(`Called send from web`);
+    console.log(req);
+    res.send('Complete')
+});
+
 app.listen(port, function () {
     console.log(`App listening`);
 });
@@ -98,80 +100,128 @@ function instapaper_to_pdf() {
         page.articles.forEach(function (article) {
             console.log(`https://www.instapaper.com${article.url}`);
             const slugRemove = /[$*_+~.,/()'"!\-:@]/g;
-            filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            file = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}`;
+            filename = `${file}.pdf`;
             filepath = `./pdfs/${filename}`;
 
-            // Create the parameters for calling listObjects
-            var bucketParams = {
-                Bucket: process.env.S3_BUCKET_NAME, Key: filename
-            };
-
-            s3.headObject(bucketParams, function (err, metadata) {
-                if (err && err.code === 'NotFound') {
-                    filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            os.execCommand(`./rmapi find . ${file}`, function (returnvalue) {
+                if(!`${returnvalue}`.includes('/'))
+                {
+                    file = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}`;
+                    filename = `${file}.pdf`;
                     filepath = `./pdfs/${filename}`;
+                    console.log(`${file} isn't on rM device`);
                     var stream = wkhtmltopdf(`https://www.instapaper.com${article.url}`, { output: `${filepath}`, cookie: [
-                            [`pfp`, `${process.env.INSTAPAPER_PFP}`], [`pfu`, `${process.env.INSTAPAPER_PFU}`], [`pfh`, `${process.env.INSTAPAPER_PFH}`]
-                        ],
-                        javascriptDelay: 2000
-                    }).on('close', function (response){
-                        filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
-                        filepath = `./pdfs/${filename}`;
-                        console.log(`stored ${filename}`);
-
-                        os.execCommand(`./rmapi put ${filepath}`, function (returnvalue) {
-                            console.log(`uploaded to rM`)
-                        });
-
-                        //STORE IN S3
-                        var fileStream = fs.createReadStream(filepath);
-                        fileStream.on('error', function(err) {
-                            console.log('File Error', err);
-                        });
-                        var uploadParams = {Bucket: process.env.S3_BUCKET_NAME, Key: filename, Body: fileStream};
-
-                        // call S3 to retrieve upload file to specified bucket
-                        s3.upload (uploadParams, function (err, data) {
-                            if (err) {
-                                console.log("Error", err);
-                            } if (data) {
-                                console.log("Upload Success", data.Location);
-
-                                // filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
-                                // filepath = `./pdfs/${filename}`;
-
-                                    //EMAIL TO KINDLE
-                                const message = {
-                                    from: 'brian.e.k@gmx.com',
-                                    to: 'b1985e.k@kindle.com',
-                                    subject: 'convert rM_send',
-                                    attachments: [
-                                        {
-                                            path: filepath
-
-                            }
+                                        [`pfp`, `${process.env.INSTAPAPER_PFP}`], [`pfu`, `${process.env.INSTAPAPER_PFU}`], [`pfh`, `${process.env.INSTAPAPER_PFH}`]
                                     ],
-                                    text: 'See attachment'
-                                };
+                                    javascriptDelay: 2000
+                                }).on('close', function (response){
+                                    filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+                                    filepath = `./pdfs/${filename}`;
+                                    console.log(`stored ${filename}`);
 
-                                transporter.sendMail(message, (error, info) => {
-                                    if (error) {
-                                        console.log(error);
-                                        res.status(400).send({success: false})
-                                    } else {
-                                        console.log('sent email')
-                                        res.status(200).send({success: true});
-                                    }
-                                });
-                            }
-                        });
-                    });
+                                    os.execCommand(`./rmapi put ${filepath}`, function (returnvalue) {
+                                        filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+                                        console.log(`${filename} uploaded to rM`)
+                                        //EMAIL TO KINDLE
+                                        const message = {
+                                            from: 'brian.e.k@gmx.com',
+                                            to: 'b1985e.k@kindle.com',
+                                            subject: 'convert rM_send',
+                                            attachments: [
+                                                { path: filepath }
+                                                ],
+                                            text: 'See attachment'
+                                        };
+                                        transporter.sendMail(message, (error, info) => {
+                                            if (error) {
+                                                console.log(error);
+                                                res.status(400).send({success: false})
+                                            } else {
+                                                console.log('sent email')
+                                                res.status(200).send({success: true});
+                                            }
+                                        });
 
-                } else {
-                    filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
-                    console.log(`exists: ${filename}`)
+                                    });
+                    })
+
                 }
             });
+
+
+
+            // // Create the parameters for calling listObjects
+            // var bucketParams = {
+            //     Bucket: process.env.S3_BUCKET_NAME, Key: filename
+            // };
+            //
+            // s3.headObject(bucketParams, function (err, metadata) {
+            //     if (err && err.code === 'NotFound') {
+            //         filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            //         filepath = `./pdfs/${filename}`;
+            //         var stream = wkhtmltopdf(`https://www.instapaper.com${article.url}`, { output: `${filepath}`, cookie: [
+            //                 [`pfp`, `${process.env.INSTAPAPER_PFP}`], [`pfu`, `${process.env.INSTAPAPER_PFU}`], [`pfh`, `${process.env.INSTAPAPER_PFH}`]
+            //             ],
+            //             javascriptDelay: 2000
+            //         }).on('close', function (response){
+            //             filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            //             filepath = `./pdfs/${filename}`;
+            //             console.log(`stored ${filename}`);
+            //
+            //             os.execCommand(`./rmapi put ${filepath}`, function (returnvalue) {
+            //                 console.log(`uploaded to rM`)
+            //             });
+            //
+            //             //STORE IN S3
+            //             var fileStream = fs.createReadStream(filepath);
+            //             fileStream.on('error', function(err) {
+            //                 console.log('File Error', err);
+            //             });
+            //             var uploadParams = {Bucket: process.env.S3_BUCKET_NAME, Key: filename, Body: fileStream};
+            //
+            //             // call S3 to retrieve upload file to specified bucket
+            //             s3.upload (uploadParams, function (err, data) {
+            //                 if (err) {
+            //                     console.log("Error", err);
+            //                 } if (data) {
+            //                     console.log("Upload Success", data.Location);
+            //
+            //                     // filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            //                     // filepath = `./pdfs/${filename}`;
+            //
+            //                         //EMAIL TO KINDLE
+            //                     const message = {
+            //                         from: 'brian.e.k@gmx.com',
+            //                         to: 'b1985e.k@kindle.com',
+            //                         subject: 'convert rM_send',
+            //                         attachments: [
+            //                             {
+            //                                 path: filepath
+            //
+            //                 }
+            //                         ],
+            //                         text: 'See attachment'
+            //                     };
+            //
+            //                     transporter.sendMail(message, (error, info) => {
+            //                         if (error) {
+            //                             console.log(error);
+            //                             res.status(400).send({success: false})
+            //                         } else {
+            //                             console.log('sent email')
+            //                             res.status(200).send({success: true});
+            //                         }
+            //                     });
+            //                 }
+            //             });
+            //         });
+            //
+            //     } else {
+            //         filename = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}.pdf`;
+            //         console.log(`exists: ${filename}`)
+            //     }
+            // });
 
         })
     });
