e80a9072862144b5153968b7068733364fd7edb9
brian kaemingk <brian.e.k@gmail.com>
2019-12-04 17:49:51 -0800

    Fixing title

--- index.js
+++ index.js
@@ -89,7 +89,6 @@ app.get('/', function (req, res) {
         }
     }).then(page => {
         //console.log(page.articles);
-        console.log(`ebook-convert ./pdfs/${file}.epub ./pdfs/${file}.mobi --title "${article.title}" --output-profile kindle_pw3 --mobi-file-type both --sr1-search '<div class="calibre_navbar">(.|\n)*?</div>'`);
 
             page.articles.forEach(function (article) {
             file = `${slugify(article.title, {replacement: '-', remove: slugRemove, lower: true})}`;
@@ -125,7 +124,7 @@ app.get('/', function (req, res) {
                                     filepath = `./pdfs/${filename}`;
                                     console.log("Completed rM upload");
 
-                                    os.execCommand(`ebook-convert ./pdfs/${file}.epub ./pdfs/${file}.mobi --title "${article.title}" --output-profile kindle_pw3 --mobi-file-type both --sr1-search '<div class="calibre_navbar">(.|\n)*?</div>'`, function (returnvalue) {
+                                    os.execCommand(`ebook-convert ./pdfs/${file}.epub ./pdfs/${file}.mobi --title "${article.title}" --asciiize --output-profile kindle_pw3 --mobi-file-type both --sr1-search '<div class="calibre_navbar">(.|\n)*?</div>'`, function (returnvalue) {
                                         file = `${slugify(article.title, {
                                             replacement: '-',
                                             remove: slugRemove,
